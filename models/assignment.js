f1bbe783cde409c064e0d8abf50bbaaf669b44a4
Juan Carlos <juancafe2@gmail.com>
2016-08-08 10:50:50 -0700

    edit assignment added

--- models/assignment.js
+++ models/assignment.js
@@ -55,6 +55,8 @@ exports.delete = function(id) {
 
 //Updates the assignment
 exports.update = function(id, updateObj) {
+  if (updateObj.total) updateObj.total = parseInt(updateObj.total)
+  if (updateObj.score) updateObj.total = parseInt(updateObj.score)
   return new Promise((resolve, reject) => {
     let sql = squel.update()
                 .table('')
